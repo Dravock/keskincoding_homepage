25c23e92fe9aa0954a32082e60a497c515cf2fcb
Deryan Keskin <Deryan@keskin-neubulach.de>
2023-04-27 21:59:18 +0200

    changed svg from own js file to fa

--- src/Pages/Public/App.js
+++ src/Pages/Public/App.js
@@ -6,6 +6,7 @@ import HeroIMG from '../../includes/img/jpeq/connected_world.jpeg'
 // IMG IMPORTS
 import LaptopIMG from '../../includes/img/jpeq/pexels-junior-edited.jpg'
 import codeIMG from '../../includes/img/jpeq/pexels-pixabay-code.jpg'
+import laptop_codeIMG from '../../includes/img/jpeq/pc_with_code.jpg'
 
 
 
@@ -22,15 +23,15 @@ return (
   <div className="">
     <Header />
     <Hero heroIMG={HeroIMG}/>
-    <div className='container mx-auto '>
-      <h1 className='mt-10 ml-5 sm:ml-0 font-bold text-3xl underline'>Wer bin ich?</h1>
-      <div className='grid grid-cols-1 sm:grid-cols-12 mt-5'>
+    <div className='bg-white container mx-auto px-5'>
+      <div className='grid grid-cols-1 sm:grid-cols-12  pt-5 '>      
         <div className=' col-span-6 order-last sm:order-2'>
-          <div className='sm:flex sm:items-center h-full'>
-            <span className='inline-block align-middle mt-0 md:-mt-3 lg:mt-4 px-4 sm:px-0 sm:mr-3 text-justify'>
-              Herzlich Willkommen auf meiner Seite! Ich heiße Deryan Keskin und komme aus dem Kreis Calw in Baden-Württemberg, 
-              ich bin 32 Jahre Alt und habe vor 5 Jahren meine Ausbildung zum Fachinformatiker für Anwendungsentwicklung abgeschlossen. 
-              Hauptsächlich bin ich in der Webentwicklung tätig aber auch andere Anwendungen und Bereiche sind mir geläufig. 
+          <h1 className='w-full pt-0 sm:pt-5 ml-5 sm:ml-0 font-bold text-3xl underline '>Wer bin ich?</h1>
+          <div className='sm:flex sm:items-center md:items-start h-full'>
+            <span className='inline-block align-middle mt-0  lg:mt-4 px-4 sm:px-0 sm:mr-3 text-justify'>
+              Ich heiße Deryan Keskin und komme aus dem Kreis Calw in Baden-Württemberg, 
+              vor 5 Jahren habe ich meine Ausbildung zum Fachinformatiker für Anwendungsentwicklung abgeschlossen. 
+              Hauptsächlich bin ich in der Webentwicklung tätig aber auch andere Anwendungsbereiche sind mir geläufig. 
               Am liebsten Entwickle ich Web-Applikationen in React.js.
             </span>
           </div>
@@ -44,7 +45,7 @@ return (
         <div className=' col-span-6 order-1 sm:order-last'>
           <div className='mt-5'>
             <p className='ml-4 font-bold'>Progammiersprachen die ich gelernt habe:</p>
-            <ul className='mt-5 ml-14' style={{listStyleType:"square"}}>
+            <ul className='mt-2 ml-14' style={{listStyleType:"square"}}>
               <div className='grid grid-cols-12'>
                 <div className='col-span-6'>
                   <li>HTML</li>
@@ -66,6 +67,14 @@ return (
         </div>
         <div className='col-span-6 '>
           <img className='px-4 sm:px-0 md:aspect-video h-full' src={codeIMG} alt="laptop"  width={"100%"}/>
+        </div>
+      </div>
+      <hr className='my-10 bg-black'/>
+      <div className='bg-blue-700/80 w-full py-10'>
+        <h2 className='text-center font-bold'>Konnte ich ihr Intresse wecken?</h2>
+        <h3 className='text-center font-bold'>Dann Kontieren Sie mich ganz einfach über mein Kontaktformular</h3>
+        <div className='text-center'>
+          <button className='btn-red'>Kontaktformular</button>
         </div>
       </div>
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Cookies from 'universal-cookie'
 import Logo from '../../includes/img/png/KeskinCoding-OhneBG.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 function Header() {
 
@@ -22,6 +25,22 @@ const logout =()=>{
     navigate('/')
 }
 
+const openMobileNav =()=>{
+    const navOpen = document.getElementById("mobile-nav-open")
+    const body = document.body
+    body.classList.add("overflow-hidden")
+    navOpen.classList.add("block")
+    navOpen.classList.remove("hidden")
+}
+
+const closeMobileNav =()=>{
+    const navOpen = document.getElementById("mobile-nav-open")
+    const body = document.body
+    body.classList.remove("overflow-hidden")
+    navOpen.classList.add("hidden")
+    navOpen.classList.remove("block")
+}
+
 return (
     <div className='flex justify-between  bg-blue-500/40'>
         <div className='flex items-center'>
@@ -31,8 +50,25 @@ return (
         <div className=''>
             
         </div>
-        <div className='flex pb-2'>
-            <div className='hidden sm:flex  mt-8 '>     
+        <div className=' flex pb-2'>
+            <div className='sm:hidden mt-3 mr-4 cursor-pointer' >
+                <FontAwesomeIcon icon={faBars} className="absolute right-3 top-3 text-black justify-end" size='3x' onClick={()=>openMobileNav()}/>
+
+            </div>
+            <div id="mobile-nav-open" className='absolute h-[101%]  bg-black/95 z-30 top-0 right-0 w-full hidden'>
+                <div className='flex justify-center'>
+                    <h2 className=' text-white  text-2xl font-bold mt-4 '>KSS</h2>
+                    <FontAwesomeIcon icon={faXmark} className="absolute right-3 top-2 text-white justify-end" size='3x' onClick={()=>closeMobileNav()}/>
+                </div>
+                <div className='grid grid-cols-12 text-white mt-20'>
+                    <button className='col-span-full py-6 text-lg md:text-xl font-bold mx-2 '>Home</button>
+                    <button className='col-span-full py-6 text-lg md:text-xl font-bold mx-2 '>Meine Projekte</button>
+                    <button className='col-span-full py-6 text-lg md:text-xl font-bold mx-2 '>Services</button>
+                    <button className='col-span-full py-6 text-lg md:text-xl font-bold mx-2 '>Contact us</button>
+                </div>
+            </div>
+
+            <div id="desktop-nav-bar" className='hidden sm:flex  mt-8 '>     
                 <button className='text-lg md:text-xl font-bold mx-2 hover-underline-animation'>Home</button>
                 <button className='text-lg md:text-xl font-bold mx-2 hover-underline-animation'>Meine Projekte</button>
                 <button className='text-lg md:text-xl font-bold mx-2 hover-underline-animation'>Services</button>
